27b2cc4c1bcc8bf79d81c46509ab2aba3d784f58
lorandnagy97 <56968310+lorandnagy97@users.noreply.github.com>
2021-04-04 19:28:03 +0300

    Create ToolNoGameObject.cs

---
+++ Code/ToolNoGameObject.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.Messaging;
+
+namespace Tools
+{
+    public class ToolNoGameObject : Tool
+    {
+        public GameObject PathObject;
+        Vector3 GetPathLocation() {
+            Vector3 newPos = transform.parent.parent.position;
+            newPos.y = newPos.y - transform.parent.parent.GetComponent<BoxCollider>().size.y+2f;
+            return newPos;
+        }
+        
+        Quaternion GetPathRotation() {
+            Quaternion newRot = transform.parent.rotation;
+            newRot *= Quaternion.Euler(0, 180f, 0);
+            return newRot;
+        }
+
+        [ServerRpc]
+        void SubmitSpawnPathRequestServerRpc()
+        {
+            GameObject path = Instantiate(PathObject, GetPathLocation(), GetPathRotation());
+            path.GetComponent<NetworkObject>().Spawn();
+        }
+
+        void SpawnPath() {
+            SubmitSpawnPathRequestServerRpc();
+        }
+        
+        public override void Use() {
+            SpawnPath();
+        }
+    }
+}
